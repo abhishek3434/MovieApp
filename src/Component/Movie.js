1b572faed94712e997bb2c83ecd6de79b11b86bb
Abhishek Joshi <abhishekjoshi433@gmail.com>
2023-03-28 03:20:35 +0530

    Card updation

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -21,7 +21,6 @@ class Movie extends Component {
                     <div class="card-body">
                       <h5 class="card-title">{movieObj.title}</h5>
                       <p class="card-text">{movieObj.overview}</p>
-                      {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
                     </div>
                   </div>
                 ))
