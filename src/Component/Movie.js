4d0a103f96ae716ff2d4fa8c74d78b2e8c8e5347
Abhishek Joshi <abhishekjoshi433@gmail.com>
2023-03-29 02:40:42 +0530

    Added Favourite buttom and hover property

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { movies } from "./getMovieData";
 class Movie extends Component {
+  constructor() {
+    super();
+    this.state = { hover: "" };
+  }
   render() {
     let data = movies["results"];
 
@@ -13,18 +17,41 @@ class Movie extends Component {
     ) : (
       <div>
         <h3 className="text-center">Trending Movies</h3>
-        <div style={{display:"flex",flexWrap:'wrap',justifyContent:'space-around'}}>
-            {
-                data.map((movieObj)=>(
-                    <div className="card" key={movieObj.id} style={{width: "18rem",margin:"1rem"}}>
-                    <img className="card-img-top" src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`} alt={movieObj.title} />
-                    <div className="card-body">
-                      <h5 className="card-title">{movieObj.title}</h5>
-                      <p className="card-text ">{movieObj.overview}</p>
-                    </div>
-                  </div>
-                ))
-            }
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {data.map((movieObj) => (
+            <div
+              className="card movie-card"
+              key={movieObj.id}
+              style={{ width: "18rem", margin: "1rem" }}
+              onMouseEnter={()=>this.setState({ hover: movieObj.id })}
+              onMouseLeave={()=>this.setState({ hover: "" })}
+            >
+              <img
+                className="card-img-top"
+                src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`}
+                alt={movieObj.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movieObj.title}</h5>
+                <p className="card-text ">{movieObj.overview}</p>
+              </div>
+              <div className="button-flex">
+                {this.state.hover === movieObj.id ? (
+                  <button className="btn btn-primary movie-button">
+                    Add to favourite
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     );
