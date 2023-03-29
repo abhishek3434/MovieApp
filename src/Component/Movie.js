e3f89493289055144f394f96fd2d28c6db0f7562
Abhishek Joshi <abhishekjoshi433@gmail.com>
2023-03-29 12:20:01 +0530

    Pagination

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 // import { movies } from "./getMovieData";
-import axios from 'axios';
+import axios from "axios";
 
 class Movie extends Component {
   constructor() {
     super();
-    this.state = { hover: "",movies:[] };
+    this.state = { hover: "", movies: [],parr:[1],currentPage:1};
   }
-  async componentDidMount(){
+  async componentDidMount() {
     // https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a3ata1c9&language=en-US&page=${newPage}
-    const response=await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=3')
-    const data=response.data;
-    this.setState({movies:[...data.results]});
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currentPage}`
+    );
+    const data = response.data;
+    this.setState({ movies: [...data.results] });
   }
+
+  
+
+
   render() {
     let data = this.state.movies;
 
-    return data === "" ? (
+    return data.length === 0 ? (
       <>
         <div className="spinner-border" role="status">
           <span className="sr-only"></span>
@@ -37,8 +43,8 @@ class Movie extends Component {
               className="card movie-card"
               key={movieObj.id}
               style={{ width: "18rem", margin: "1rem" }}
-              onMouseEnter={()=>this.setState({ hover: movieObj.id })}
-              onMouseLeave={()=>this.setState({ hover: "" })}
+              onMouseEnter={() => this.setState({ hover: movieObj.id })}
+              onMouseLeave={() => this.setState({ hover: "" })}
             >
               <img
                 className="card-img-top"
@@ -61,6 +67,38 @@ class Movie extends Component {
             </div>
           ))}
         </div>
+
+        <div className="div-pagination">
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href="">
+                  Previous
+                </a>
+                </li>
+           {
+            this.state.parr.map((value)=>(
+              <div key={value}> 
+              <li className="page-item">
+                <a className="page-link" href="">
+                  {value}
+                </a>
+              </li>
+              </div>
+            ))
+           }
+             
+           
+              <li className="page-item">
+                <a className="page-link" href="">
+                  Next
+                </a>
+              </li>
+            </ul>
+
+          </nav>
+        </div>
+
       </div>
     );
   }
